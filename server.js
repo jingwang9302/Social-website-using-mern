cd56cff46a0f8a7d84dfcd6dd4cf51d1de85b502
JingWang <jingwang9302@gmail.com>
2020-05-20 23:22:21 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API Running"));
+
+// initial the port to access, default to 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
